8287193260f8e6505ae5dbf398ef9d74836b8656
twinklymeat <twinklymeat8990@gmail.com>
2024-09-21 01:18:23 -0400

    update 1.1
    Rotated blocks (glass, spikes, triggers, etc...) now work and decorations now work and can be toggled with D. Triggers can also be toggled with T, and checkpoint triggers are now shown and can be toggled with C.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import json
 import codecs
 import pygame
-
+from math import *
 name = input("File: ")
 pygame.init()
 
@@ -10,12 +10,14 @@ SCREEN = pygame.display.set_mode(resolution, pygame.RESIZABLE)
 
 SCREEN.fill((0,0,0))
 
+pygame.display.set_caption(name)
 
 pygame.display.flip()
 
 file = codecs.open(name, encoding= "utf-8-sig")
 text = file.read()
 data = json.loads(text)
+
 
 # print(data)
 
@@ -36,32 +38,51 @@ objectHWName = []
 
 def getObjects(layers):
     for i in layers:
-        print(i["className"])
+        # print(i["className"])
         x = int(i["params"]["x"])
         y = int(i["params"]["y"])
 
-        print(x,y)
+        # print(x,y)
         globalPos = []
         try: 
             for v in i["params"]["vertices"]:
-                print("X/Y",v["x"], v["y"])
+                # print("X/Y",v["x"], v["y"])
                 localx = int(v["x"])
                 localy = int(v["y"])
                 globalPos.append([localx + x, localy + y])
             objectList.append(globalPos)
             objectName.append(i["className"])
         except:
-            print("H/W",i["params"]["height"], i["params"]["width"])
-            objectListHW.append((x,y,i["params"]["height"], i["params"]["width"]))
+            # print("H/W",i["params"]["height"], i["params"]["width"])
+            objectListHW.append((x,y,i["params"]["height"], i["params"]["width"],i["params"]["rotation"]))
             objectHWName.append(i["className"])
-        print(globalPos)
-        print("\n")
+        # print(globalPos)
+        # print("\n")
     return objectList, objectListHW, objectName, objectHWName
 
-decLayer = data["layers"][2]
+decLayer = data["layers"][1]
 layer5 = data["layers"][5]
-decs = getObjects(decLayer)
-triggers = getObjects(layer5)
+checkpoints = data["layers"][4]
+tnt = data["layers"][2]
+mystery = data["layers"][3]
+
+if decLayer != None:
+    decs = getObjects(decLayer)
+if layer5 != None:
+    triggers = getObjects(layer5)
+if tnt != None:
+    tntLayer = getObjects(tnt)
+if mystery != None:
+    mysteryLayer = getObjects(mystery)
+# try:
+#     print("2nd Decoration Successful!")
+# except:
+    # pass
+if checkpoints != None:
+    savepoints = getObjects(checkpoints)
+
+# try:
+
 OBJ = getObjects(layers)
 
 objectList = OBJ[0]
@@ -81,9 +102,13 @@ moveV = [0,0]
 zoomV = 1
 timer = pygame.time.Clock()
 for item in objectList:
-    if objectName[objectList.index(item)] == "frg.game.editor.objects::LandscapeShaper":
+    if objectName[objectList.index(item)] in ["frg.game.editor.objects::LandscapeShaper", "frg.game.editor.objects::FinishShaper", "frg.game.editor.objects::WaterShaper"]:
         item.append(item[0])
 
+showTriggers = True
+showCheck = True
+showDecs = False
+debug = False
 while True:
     timer.tick(60)
     SCREEN.fill((0,0,0))
@@ -92,23 +117,85 @@ while True:
         surface = pygame.surface.Surface((v[3]*zoomV,v[2]*zoomV))
         rec = pygame.rect.Rect(zoomV*(v[0] + moveV[0]), zoomV*(v[1] + moveV[1]), v[3]*zoomV, v[2]*zoomV)
         rec.center = (zoomV*(v[0] + moveV[0]), zoomV*(v[1] + moveV[1]))
-        if objectHWName[objectListHW.index(v)] == "FinishZone":
-            surface.fill((50,255,50,100))
+        """
+        v[0] = x
+        v[1] = y
+        v[2] = width
+        v[3] = height
+        v[4] = rotation
+        """
+        if not showTriggers:
+            if objectHWName[objectListHW.index(v)] in ["ToggleE", "EggE", "OvalTemplate","CameraZoneE"]:
+                continue
+        
+        if not showCheck:
+            if objectHWName[objectListHW.index(v)] == "SafePointE":
+                continue
+        if not showDecs:
+            if "Dec" in objectHWName[objectListHW.index(v)]:
+                continue
+        # print(v)
+        color = ()
+        if  objectHWName[objectListHW.index(v)] == "FinishZone":
+            color = (50,255,50)
         elif objectHWName[objectListHW.index(v)] == "PivotJointE":
-            surface.fill((100,100,100,100))
+            color = (100,100,100,100)
         elif objectHWName[objectListHW.index(v)] == "ToggleE":
-            surface.fill((255,255,100,100))
+            color = (255,255,100,100)
         elif objectHWName[objectListHW.index(v)] == "MotorJointE":
-            surface.fill((100,255,255,100))
+            color = (100,255,255,100)
         elif objectHWName[objectListHW.index(v)] == "Tnt1":
-            surface.fill((120,60,0,150))
+            color = (120,60,0,150)
         elif objectHWName[objectListHW.index(v)] == "BarrelE":
-            surface.fill((200,200,200))
-        elif objectHWName[objectListHW.index(v)] == "EggE":
-            surface.fill((255,255,255))
+            color = (200,200,200)
+        elif objectHWName[objectListHW.index(v)] == "EggE": # I think this is to load other geometry
+            color = (255,255,255)
+        elif objectHWName[objectListHW.index(v)] == "SpikesE":
+            color = (255,0,0)
+        elif objectHWName[objectListHW.index(v)] in ["Removed0","Removed1"]: #removed0 is vertical Removed1 is horizontal
+            color = (150,80,0)
+        elif objectHWName[objectListHW.index(v)] == "PlayerWP":
+            color = (255,255,255)
+        elif objectHWName[objectListHW.index(v)] == "SafePointE":
+            color = (0,150,0)
+        elif objectHWName[objectListHW.index(v)] in ["GlassCrashed1","GlassCrashed0"]:
+            color = (0,255,255)
+        elif objectHWName[objectListHW.index(v)] == "GeyserE":
+            color = (0,0,255)
+        elif objectHWName[objectListHW.index(v)] == "BoostE":
+            color = (0,0,150)
+        elif objectHWName[objectListHW.index(v)] == "CameraZoneE":
+            color = (255,255,255)
+        elif objectHWName[objectListHW.index(v)] == "OvalTemplate":
+            color = (255,0,255)
+        elif objectHWName[objectListHW.index(v)] == "SignPillar":
+            color = (255,255,255)
+        elif objectHWName[objectListHW.index(v)] == "PlankWipe0":
+            color = (150,80,0)
+        elif "SpikeBall" in objectHWName[objectListHW.index(v)]:
+            color = (150,0,0)
+        
         else:
-            surface.fill((255,0,255,100))
-        SCREEN.blit(surface, rec)#(zoomV*(v[0] + moveV[0]),zoomV*(v[1] + moveV[1])))
+            if debug:
+                print(objectHWName[objectListHW.index(v)])
+            color = (255,0,255,100)
+        
+        x = v[0]
+        y = v[1]
+        w = v[2]
+        h = v[3]
+        r = v[4]
+
+        r= radians(r) + pi/2
+        # print(r)
+        topL = (int(((w/2)*cos(r)-(h/2)*sin(r))*zoomV),int(((w/2)*sin(r)+(h/2)*cos(r))*zoomV))
+        topR = (int(-((w/2)*cos(r)+(h/2)*sin(r))*zoomV),int(-((w/2)*sin(r)-(h/2)*cos(r))*zoomV))
+        bottomL = (-1*topR[0], -1*topR[1])
+        bottomR = (-1*topL[0], -1*topL[1])
+
+        pygame.draw.lines(SCREEN,color,False, ((rec.center[0]+topR[0],rec.center[1]+topR[1]), (rec.center[0]+topL[0],rec.center[1]+topL[1]), (rec.center[0]+bottomL[0], rec.center[1]+bottomL[1]), (rec.center[0]+bottomR[0],rec.center[1]+bottomR[1]), (rec.center[0]+topR[0],rec.center[1]+topR[1]), (rec.center[0]+bottomL[0], rec.center[1]+bottomL[1])))
+
+
 
     for item in objectList:
         vOG = item[0]
@@ -126,8 +213,13 @@ while True:
 # 
 # MoverPather: Cyan
 # 
+# WaterShaper: Blue
+# 
+# DevPather: White # ???????
+# 
 # Others: white
-            SCREEN.blit(vertex, [(v[0] + moveV[0])*zoomV,(v[1] + moveV[1])*zoomV])
+            
+            SCREEN.blit(vertex, [(v[0] + moveV[0])*zoomV - 1,(v[1] + moveV[1])*zoomV - 1])
             if objectName[objectList.index(item)] == "frg.game.editor.objects::LandscapeShaper":
                 pygame.draw.line(SCREEN,pygame.color.Color(255,0,0,255),[int(zoomV*(vOG[0] + moveV[0])),int((vOG[1]+moveV[1])*zoomV)],[int((v[0] + moveV[0])*zoomV), int((v[1] + moveV[1])*zoomV)])
             elif objectName[objectList.index(item)] == "frg.game.editor.objects::DynamicPather":
@@ -138,6 +230,12 @@ while True:
                 pygame.draw.line(SCREEN,pygame.color.Color(255,0,255,255),[int(zoomV*(vOG[0] + moveV[0])),int((vOG[1]+moveV[1])*zoomV)],[int((v[0] + moveV[0])*zoomV), int((v[1] + moveV[1])*zoomV)])
             elif objectName[objectList.index(item)] == "frg.game.editor.objects::MoverPather":
                 pygame.draw.line(SCREEN,pygame.color.Color(100,255,255,100),[int(zoomV*(vOG[0] + moveV[0])),int((vOG[1]+moveV[1])*zoomV)],[int((v[0] + moveV[0])*zoomV), int((v[1] + moveV[1])*zoomV)])
+            elif objectName[objectList.index(item)] == "frg.game.editor.objects::WaterShaper":
+                pygame.draw.line(SCREEN,pygame.color.Color(0,0,255,100),[int(zoomV*(vOG[0] + moveV[0])),int((vOG[1]+moveV[1])*zoomV)],[int((v[0] + moveV[0])*zoomV), int((v[1] + moveV[1])*zoomV)])
+            elif objectName[objectList.index(item)] == "frg.game.editor.objects::FinishShaper":
+                pygame.draw.line(SCREEN,pygame.color.Color(0,255,0,100),[int(zoomV*(vOG[0] + moveV[0])),int((vOG[1]+moveV[1])*zoomV)],[int((v[0] + moveV[0])*zoomV), int((v[1] + moveV[1])*zoomV)])
+            elif objectName[objectList.index(item)] == "frg.game.editor.objects::DevPather":
+                pygame.draw.line(SCREEN,pygame.color.Color(255,255,255),[int(zoomV*(vOG[0] + moveV[0])),int((vOG[1]+moveV[1])*zoomV)],[int((v[0] + moveV[0])*zoomV), int((v[1] + moveV[1])*zoomV)])
             else:
                 print(objectName[objectList.index(item)])
                 pygame.draw.line(SCREEN,pygame.color.Color(255,255,255,255),[int(zoomV*(vOG[0] + moveV[0])),int((vOG[1]+moveV[1])*zoomV)],[int((v[0] + moveV[0])*zoomV), int((v[1] + moveV[1])*zoomV)])
@@ -146,13 +244,13 @@ while True:
     keys = pygame.key.get_pressed()
 
     if keys[pygame.K_RIGHT]:
-        moveV[0] -= 40
+        moveV[0] -= 20 * (1/zoomV)
     elif keys[pygame.K_LEFT]:
-        moveV[0] += 40
+        moveV[0] += 20 * (1/zoomV)
     if keys[pygame.K_UP]:
-        moveV[1] += 40
+        moveV[1] += 20 * (1/zoomV)
     if keys[pygame.K_DOWN]:
-        moveV[1] -= 40
+        moveV[1] -= 20 * (1/zoomV)
     # if keys[pygame.K_EQUALS]:
     #     print("+")
     #     zoomV = zoomV * 2
@@ -170,6 +268,29 @@ while True:
             if event.key == pygame.K_MINUS:
                 print("-")
                 zoomV = zoomV * .5
+            if event.key == pygame.K_t:
+                if showTriggers:
+                    showTriggers = False
+                else:
+                    showTriggers = True
+
+            if event.key == pygame.K_c:
+                if showCheck:
+                    showCheck = False
+                else:
+                    showCheck = True
+            if event.key == pygame.K_d:
+                if keys[pygame.K_LCTRL] or keys[pygame.K_RCTRL]:
+                    if debug:
+                        debug = False
+                    else:
+                        debug = True
+                    continue
+                if showDecs:
+                    showDecs = False
+                else:
+                    showDecs = True
+            
     pygame.display.flip()
 
-print()
+# print()
